37ca7b071c6dba2d89bfb8969f010ffb228a7c3e
jwmccowan <github@jwmccowan.com>
2023-02-26 00:01:55 +1100

    add navbar to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,2 @@
-<div class="container mx-auto px-4">
-	<h1 class="text-3xl font-bold py-6">Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
+<h1 class="text-3xl font-bold py-6">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
