28abd8c76560b64a1eb85250d5339fae36b3330d
jwmccowan <github@jwmccowan.com>
2023-02-25 23:06:29 +1100

    install and config tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import './tailwind.css';
+	import './globals.css';
+</script>
+
+<div class="container mx-auto px-4">
+	<h1 class="text-3xl font-bold py-6">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['src/**/*.{ts,js,svelte,html}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
