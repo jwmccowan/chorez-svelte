45863f92ea6e5f2f18e2ab4da7a644d3b58d5a5d
jwmccowan <github@jwmccowan.com>
2023-02-26 00:45:55 +1100

    implement log in with username

---
+++ src/routes/log-in/+page.server.ts
@@ -0,0 +1,38 @@
+import { error, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+import { auth } from '../../auth/lucia.server';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load = (async (event) => {
+	const session = await event.locals.validate();
+	if (session) {
+		throw redirect(301, '/');
+	}
+}) satisfies PageServerLoad;
+
+const LogInFormSchema = z.object({ username: z.string(), password: z.string().min(8) });
+
+export const actions = {
+	async default(event) {
+		// Parse and validate form data
+		const parseResult = LogInFormSchema.safeParse(event.locals.formData);
+		if (!parseResult.success) {
+			throw error(400, 'Invalid credentials');
+		}
+
+		// Validate user and set session
+		try {
+			const key = await auth.validateKeyPassword(
+				'username',
+				parseResult.data.username,
+				parseResult.data.password
+			);
+			const session = await auth.createSession(key.userId);
+			event.locals.setSession(session);
+		} catch (e) {
+			throw error(400, 'Invalid credentials');
+		}
+
+		throw redirect(302, '/');
+	}
+} satisfies Actions;
